a96b94760db07f0789db7bab5a0b3f506be72af9
Ash Anand <cppanand@gmail.com>
2025-05-15 11:26:26 -0400

    adjust logo styling

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -4,8 +4,9 @@ const Navbar = () => {
   return (
     <aside className="fixed left-0 top-0 h-dvh w-64 bg-white shadow-lg">
       <div className="flex flex-col h-full p-4">
-        <div className="flex justify-center mb-8">
+        <div className="flex flex-row items-center mb-8">
           <img src="/logo.png" alt="Pennie Logo" className="h-16 w-auto object-contain" />
+          <p className="font-sans text-2xl font-bold text-black">Pennie</p>
         </div>
         <nav className="flex flex-col space-y-4">
           <NavbarButton text="Home" className="w-full" />

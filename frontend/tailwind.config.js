a0e6cd7943f65c128d2aefeee019f76ff9b8514c
Ash Anand <cppanand@gmail.com>
2025-05-15 12:31:40 -0400

    add pennie color palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pennie: {
+          50: '#fff7ed',
+          100: '#ffedd5',
+          200: '#fdd3a4',
+          300: '#fcb975',
+          400: '#f9913e',
+          500: '#f77118',
+          600: '#e8570e',
+          700: '#c0410e',
+          800: '#993413',
+          900: '#7b2d13',
+          950: '#421408',
+        },
+      },
+    },
   },
   plugins: [],
 };

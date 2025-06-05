d19c5b73958bffa65ef7bde30a5e84c57b9bc1c2
Ash Anand <cppanand@gmail.com>
2025-06-05 12:55:05 -0400

    add openAI util function for expense data extraction

--- backend/src/utils/openAI.ts
+++ backend/src/utils/openAI.ts
@@ -3,3 +3,58 @@ import { zodTextFormat } from 'openai/helpers/zod';
 import { z } from 'zod';
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+const ExpenseSchema = z.object({
+  name: z.string(),
+  cost: z.number(),
+  category: z.string(),
+});
+
+const expenseSystemPrompt = `
+    You are an assistant for an expense tracking application. 
+    When given a message describing an expense, extract the cost, name of the expense, and categorize the expense. 
+    If cost or name is missing, return an error. 
+    Example: '$5 coffee' will return cost $5, name: 'Coffee", and category: 'Coffee'.
+    Use proper capitalization for the name and category.
+    Some identifiers for categories are:
+
+    - Food
+    - Groceries
+    - Restaurants
+    - Drinks
+    - Alcohol
+    - Coffee
+    - Tea
+    - Snacks
+    - Transportation
+    - Housing
+    - Utilities
+    - Entertainment
+    - Shopping
+    - Subscription
+    - Health
+    - Gifts
+    - Education
+    - Travel
+    - Other
+
+    If an expense does not fit into any of the categories, infer the category based on the name of the expense.
+`;
+
+const parseExpense = async (message: string) => {
+  const response = await openai.responses.parse({
+    model: 'gpt-4o-mini',
+    input: [
+      {
+        role: 'system',
+        content: expenseSystemPrompt,
+      },
+      { role: 'user', content: message },
+    ],
+    text: {
+      format: zodTextFormat(ExpenseSchema, 'expense_schema'),
+    },
+  });
+
+  return response.output_parsed;
+};

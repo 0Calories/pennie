392546b8bf91fd9347988444cfaf0725dbc8813f
Ash Anand <cppanand@gmail.com>
2025-06-07 15:10:00 -0400

    add tabler plane icon

--- frontend/src/components/ExpenseInput.tsx
+++ frontend/src/components/ExpenseInput.tsx
@@ -1,6 +1,6 @@
-import { PaperPlaneIcon } from '@radix-ui/react-icons';
 import { useState } from 'react';
 import { IconButton } from './core/button/IconButton';
+import { IconPlane } from './core/icons/IconPlane';
 import TextInput from './core/textinput/TextInput';
 
 interface ExpenseInputProps {
@@ -25,7 +25,7 @@ export default function ExpenseInput({ onSubmit }: ExpenseInputProps) {
     <div className="fixed bottom-0 left-48 right-0 bg-white dark:bg-slate-900 border-t border-slate-200 dark:border-slate-700 p-4">
       <form onSubmit={handleSubmit} className="max-w-4xl mx-auto flex items-center gap-2">
         <TextInput value={input} onChange={setInput} placeholder="Add an expense" />
-        <IconButton icon={<PaperPlaneIcon />} onClick={handleSubmit} />
+        <IconButton icon={<IconPlane />} onClick={handleSubmit} />
       </form>
     </div>
   );
